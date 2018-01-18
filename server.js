2bc302b0756ea319ee44e4e7467b0eef29e113a8
jamesm2w <jamesmacerwright@gmail.com>
2018-01-18 19:40:20 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static("util"));
+
+// Simple root route
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
