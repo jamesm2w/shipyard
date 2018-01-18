7282be0a92bdf70f816927f476b38f54bd4aaa33
jamesm2w <jamesmacerwright@gmail.com>
2018-01-18 19:11:33 +0000

    Create main.js
    Added code from glitch site

---
+++ js/main.js
@@ -0,0 +1,89 @@
+function getQueryVariable (variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){return pair[1];}
+  }
+  return(false);
+}
+
+function cFs(status){
+  if(status == "up"){
+    return "#76ff03"; // Server is ONLINE
+  } else if (status == "maintenance") {
+    return "#d50000";
+  } else if (status == "low") {
+    return "#fff176"; //Low Population
+  } else if (status == "medium") {
+    return "#ffc107"; // Medium Population
+  } else if (status == "high") {
+    return "#e65100";
+  } else {
+    return "#ff5722"; // Some other value (deep orange)
+  }
+}
+
+function showSnackbar(message) {
+    var x = document.getElementById("snackbar");
+    x.innerText = message;
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function switchPage(next) {
+  let nextPage = document.getElementById(next), prevPage = document.querySelector(".activePage");
+  if (nextPage.id == prevPage.id) {
+    return false;
+  }
+  
+  document.getElementById("mobile-nav-" + next).className += " active";
+  document.getElementById("mobile-nav-" + prevPage.id).className = "mobile-nav-link";
+  document.getElementById("nav-" + next).className += " active";
+  document.getElementById("nav-" + prevPage.id).className = "nav-link";
+  
+  nextPage.className += " activePage";
+  prevPage.className = "section";
+
+  closeNav();
+}
+
+function setHeader(xhr) {
+  xhr.setRequestHeader('x-origin', 'bringbackthestreams');
+}
+
+function getdata() {
+  $.ajax({
+    url: "https://wa.glitch.me/api/now",
+    method: "GET",
+    dataType: 'json',
+    success: proccessData,
+    error: function(data) { alert(data) },
+    beforeSend: setHeader
+  });
+}
+
+function proccessData(data){
+  $("#servers").empty();
+  $.each(data, function (serverID, status){
+    $("#servers").append("\
+  <div class='col-3 wa-panel' id='"+serverID+"'>\
+    <h3 class='wa-header'>"+status["name"]+"</h3>\
+    <p>STATUS: <span style='color: "+cFs(status["status"])+"; text-transform: uppercase; font-weight: 700;'>"+status["status"]+"</span><br> POPULATION: <span style='color: "+cFs(status["population"])+"; text-transform: uppercase; font-weight: 700;'>"+status["population"]+"</span></p>\
+  </div>\
+  ");
+  });
+  let datetime = new Date().toLocaleString();
+  $("#last-checked").text(datetime);
+}
+
+function openNav() {
+  document.getElementById("mobileSidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mobileSidenav").style.width = "0";
+}
+
+getdata();
+setInterval(getdata(), 30000);
