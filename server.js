e51ec8c491ad32452493559c3023004c8579a214
jamesm2w <jamesmacerwright@gmail.com>
2018-01-19 09:23:00 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static("util"));
+app.use(express.static("public"));
 
 // Simple root route
 app.get("/", function (request, response) {
